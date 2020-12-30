1576f31d5828f9fe1758bfbcd8f3254d1739b322
alvyn le <alvyn279@gmail.com>
2020-12-30 14:21:09 -0500

    feat(bot): init gradle project

---
+++ discord-events/src/main/java/com/alvyn279/discord/DiscordEventsBot.java
@@ -0,0 +1,11 @@
+package com.alvyn279.discord;
+
+public class DiscordEventsBot {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    public static int someFunction() {
+        return 1;
+    }
+}
---
+++ discord-events/src/test/java/com/alvyn279/discord/DiscordEventsBotTest.java
@@ -0,0 +1,12 @@
+package com.alvyn279.discord;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiscordEventsBotTest {
+    @Test
+    public void someFunctionReturnsOne() {
+        assertEquals(1, DiscordEventsBot.someFunction());
+    }
+}
